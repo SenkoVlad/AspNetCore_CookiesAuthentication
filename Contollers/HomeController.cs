409d45d96d60081dd7ba58c547ed55042a262fad
SenkoVlad <vlad.senko2011@yandex.ru>
2021-04-13 19:39:02 +0300

    Add project files.

---
+++ Contollers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNet_AuthCookies.Contollers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            if (HttpContext.User.Identity.IsAuthenticated)
+                return View("Index" ,User.Identity.Name);
+            else
+                return View("Index", "not authorized");
+        }
+
+        public IActionResult About()
+        {
+            return Content("About", "Authorized");
+        }
+    }
+}
---
+++ Infrastructure/Data/UserContext.cs
@@ -0,0 +1,18 @@
+﻿using AspNet_AuthCookies.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNet_AuthCookies.Infrastructure.Data
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
